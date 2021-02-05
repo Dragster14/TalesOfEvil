c583b4b0e4207b47b4fbd1628272712a7882fff5
Dragster14 <robinjoannes@gmail.com>
2021-02-05 08:19:50 +0100

    Ajout possibilités chapitre2 + js

--- Chapitres/chapitre2.js
+++ Chapitres/chapitre2.js
@@ -1,1 +1,202 @@
 let vHistoire = document.getElementById('histoire');
+
+// - Chapitre 2 -
+function suite133() {
+    document.getElementById('opacite310').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('133').innerHTML;
+    document.getElementById('btn310').disabled = true;
+}
+
+// - La Boutique d'Occasions -
+function suite82() {
+    document.getElementById('opacite133').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('82').innerHTML;
+    document.getElementById('btn133_1').disabled = true;
+    document.getElementById('btn133_2').disabled = true;
+}
+
+function suite175() {
+    document.getElementById('opacite133').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('175').innerHTML;
+    document.getElementById('btn133_1').disabled = true;
+    document.getElementById('btn133_2').disabled = true;
+}
+
+// - Le défi -
+function suite407() {
+    document.getElementById('opacite82').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('407').innerHTML;
+    document.getElementById('btn82_1').disabled = true;
+    document.getElementById('btn82_2').disabled = true;
+}
+
+function suite88() {
+    document.getElementById('opacite82').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('88').innerHTML;
+    document.getElementById('btn82_1').disabled = true;
+    document.getElementById('btn82_2').disabled = true;
+}
+
+// - Pour qui tu te prends ? -
+function suite482() {
+    document.getElementById('opacite175').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('482').innerHTML;
+    document.getElementById('btn175_1').disabled = true;
+    document.getElementById('btn175_2').disabled = true;
+    document.getElementById('btn175_3').disabled = true;
+}
+
+function suite11() {
+    document.getElementById('opacite175').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('11').innerHTML;
+    document.getElementById('btn175_1').disabled = true;
+    document.getElementById('btn175_2').disabled = true;
+    document.getElementById('btn175_3').disabled = true;
+}
+
+// - Par la hache de Geronimo ! - & - Muahahaha... -
+function suite101() {
+    document.getElementById('opacite407').classList.remove('opacite');
+    document.getElementById('opacite88').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('101').innerHTML;
+    document.getElementById('btn407').disabled = true;
+    document.getElementById('btn88').disabled = true;
+}
+
+// - Retour à la maison -
+function suite451() {
+    document.getElementById('opacite101').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('451').innerHTML;
+    document.getElementById('btn101_1').disabled = true;
+    document.getElementById('btn101_2').disabled = true;
+}
+
+function suite258() {
+    document.getElementById('opacite101').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('258').innerHTML;
+    document.getElementById('btn101_1').disabled = true;
+    document.getElementById('btn101_2').disabled = true;
+}
+
+// - La bonne voie - & - Qu'est-ce qu'on fait ? - & - Cachés ? - & - On peut le faire - & - Et mince ! - & - Cachés... Ou pas ! -
+function suite368() {
+    document.getElementById('opacite451').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('368').innerHTML;
+    document.getElementById('btn451_1').disabled = true;
+    document.getElementById('btn451_2').disabled = true;
+    document.getElementById('btn451_3').disabled = true;
+}
+
+function suite71() {
+    document.getElementById('opacite451').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('71').innerHTML;
+    document.getElementById('btn451_1').disabled = true;
+    document.getElementById('btn451_2').disabled = true;
+    document.getElementById('btn451_3').disabled = true;
+}
+
+function suite166() {
+    document.getElementById('opacite451').classList.remove('opacite');
+    document.getElementById('opacite258').classList.remove('opacite');
+    document.getElementById('opacite368').classList.remove('opacite');
+    document.getElementById('opacite71').classList.remove('opacite');
+    document.getElementById('opacite324').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('166').innerHTML;
+    document.getElementById('btn451_1').disabled = true;
+    document.getElementById('btn451_2').disabled = true;
+    document.getElementById('btn451_3').disabled = true;
+    document.getElementById('btn258_1').disabled = true;
+    document.getElementById('btn258_2').disabled = true;
+    document.getElementById('btn368_1').disabled = true;
+    document.getElementById('btn368_2').disabled = true;
+    document.getElementById('btn368_3').disabled = true;
+    document.getElementById('btn71_1').disabled = true;
+    document.getElementById('btn71_2').disabled = true;
+    document.getElementById('btn71_3').disabled = true;
+    document.getElementById('btn324').disabled = true;
+}
+
+function suite266() {
+    document.getElementById('opacite258').classList.remove('opacite');
+    document.getElementById('opacite71').classList.remove('opacite');
+    document.getElementById('opacite166').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('266').innerHTML;
+    document.getElementById('btn258_1').disabled = true;
+    document.getElementById('btn258_2').disabled = true;
+    document.getElementById('btn71_1').disabled = true;
+    document.getElementById('btn71_2').disabled = true;
+    document.getElementById('btn71_3').disabled = true;
+    document.getElementById('btn166').disabled = true;
+}
+
+function suite324() {
+    document.getElementById('opacite368').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('324').innerHTML;
+    document.getElementById('btn368_1').disabled = true;
+    document.getElementById('btn368_2').disabled = true;
+    document.getElementById('btn368_3').disabled = true;
+}
+
+// - La porte bloquée -
+function suite97() {
+    document.getElementById('opacite266').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('97').innerHTML;
+    document.getElementById('btn266_1').disabled = true;
+    document.getElementById('btn266_2').disabled = true;
+}
+
+function suite207() {
+    document.getElementById('opacite266').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('207').innerHTML;
+    document.getElementById('btn266_1').disabled = true;
+    document.getElementById('btn266_2').disabled = true;
+}
+
+// - Vlan ! -
+function suite303() {
+    document.getElementById('opacite97').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('303').innerHTML;
+    document.getElementById('btn97_1').disabled = true;
+    document.getElementById('btn97_2').disabled = true;
+}
+
+function suite122() {
+    document.getElementById('opacite97').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('122').innerHTML;
+    document.getElementById('btn97_1').disabled = true;
+    document.getElementById('btn97_2').disabled = true;
+}
+
+// - Promenade au clair de lune -
+function suite454() {
+    document.getElementById('opacite207').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('454').innerHTML;
+    document.getElementById('btn207_1').disabled = true;
+    document.getElementById('btn207_2').disabled = true;
+    document.getElementById('btn207_3').disabled = true;
+}
+
+function suite424() {
+    document.getElementById('opacite207').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('424').innerHTML;
+    document.getElementById('btn207_1').disabled = true;
+    document.getElementById('btn207_2').disabled = true;
+    document.getElementById('btn207_3').disabled = true;
+}
+
+function suite319() {
+    document.getElementById('opacite207').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('319').innerHTML;
+    document.getElementById('btn207_1').disabled = true;
+    document.getElementById('btn207_2').disabled = true;
+    document.getElementById('btn207_3').disabled = true;
+}
+
+// - Les Livres Vampires (303) - & - Les Livres Vampires (122) -
+function suite109() {
+    document.getElementById('opacite303').classList.remove('opacite');
+    document.getElementById('opacite122').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('109').innerHTML;
+    document.getElementById('btn303').disabled = true;
+    document.getElementById('btn122').disabled = true;
+}
