c06623714444ae629d7bf00206d42ec7356646a2
Dragster14 <robinjoannes@gmail.com>
2021-09-15 20:09:25 +0200

    Chap3 possi 83 + js

--- Chapitres/chapitre3.js
+++ Chapitres/chapitre3.js
@@ -1,1 +1,13 @@
 let vHistoire = document.getElementById('histoire');
+
+function videoDaphne() {
+    window.open('http://www.talesofevil.com/wp-content/uploads/daphne-1-fra.mp4');
+}
+
+// - Chapitre 3 -
+function suite83() {
+    document.getElementById('opacite330').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('83').innerHTML;
+    document.getElementById('btn330_1').disabled = true;
+    document.getElementById('btn330_2').disabled = true;
+}
