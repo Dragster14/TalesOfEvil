32a0ddb4b754c2c15ad2e359cb0fb84651e794ec
Robin Joannes <robinjoannes@gmail.com>
2021-11-30 21:05:13 +0100

    Ajout possibilités jusqu'à 271&239+js

--- Chapitres/chapitre3.js
+++ Chapitres/chapitre3.js
@@ -11,3 +11,18 @@ function suite83() {
     document.getElementById('btn330_1').disabled = true;
     document.getElementById('btn330_2').disabled = true;
 }
+
+// - En route pour la Boutique d'Occasions -
+function suite271() {
+    document.getElementById('opacite83').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('271').innerHTML;
+    document.getElementById('btn83_1').disabled = true;
+    document.getElementById('btn83_2').disabled = true;
+}
+
+function suite239() {
+    document.getElementById('opacite83').classList.remove('opacite');
+    vHistoire.innerHTML += document.getElementById('239').innerHTML;
+    document.getElementById('btn83_1').disabled = true;
+    document.getElementById('btn83_2').disabled = true;
+}
